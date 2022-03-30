5a97e546d534edc91b1b69145ece884e783404b9
Jose Garcia <jose_garcia0306@hotmail.com>
2022-03-29 20:14:07 -0400

    Cree los enventos y estados del CreateTodoButton y TodoSearch

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,9 +1,17 @@
 import React from "react";
 import './CreateTodoButton.css'
 
-function CreateTodoButton() {
+function CreateTodoButton(props) {
+    const onclickButton = (msg) => {
+        alert(msg)
+    }
+
     return(
-        <button className="CreateTodoButton">+</button>
+        <button className="CreateTodoButton"
+        onClick={() => onclickButton('Aqui se deberia abrir el modal') }
+        >
+            +
+        </button>
     );
 };
 
